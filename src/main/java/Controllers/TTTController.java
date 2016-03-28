78737cfad6d37ad125d6417552fd0ce6463221c5
Anda <andarcabrera@gmail.com>
2016-03-28 14:35:54 -0500

    Accessed players and game classes from gem

--- src/main/java/Controllers/TTTController.java
+++ src/main/java/Controllers/TTTController.java
@@ -35,11 +35,20 @@ public class TTTController extends Controller{
         Map<String, String> params = gameInfoParser.getParams(request.getRawBody());
         String size = params.get("size");
 
-        String bootstrap =
-                "require \"" + "/Users/andacabrera29/Desktop/test.rb" +  "\"\n"+
+        String bootstrapSetup =
+                "require \"" + "/Users/andacabrera29/Desktop/tttj_gem/lib/tttj/game_setup.rb" +  "\"\n"+
                         "class Bootstrap \n" +
                         "   def execute root_object  \n" +
-                        "       " + "Hello.new" + "(root_object) \n" +
+                        "       " + "TTT::GameSetup.new" + "(root_object) \n" +
+                        "   end    \n" +
+                        "end \n" +
+                        "Bootstrap.new";
+
+        String bootstrapGame =
+                "require \"" + "/Users/andacabrera29/Desktop/tttj_gem/lib/tttj/game.rb" +  "\"\n"+
+                        "class Bootstrap \n" +
+                        "   def execute root_object, size  \n" +
+                        "       " + "TTT::Game.new" + "(root_object, size) \n" +
                         "   end    \n" +
                         "end \n" +
                         "Bootstrap.new";
@@ -48,11 +57,17 @@ public class TTTController extends Controller{
         loadPaths.add(".");
 
         runtime = JavaEmbedUtils.initialize( loadPaths );
-        rootRubyObject = JavaEmbedUtils.newRuntimeAdapter().eval( runtime, bootstrap );
+        rootRubyObject = JavaEmbedUtils.newRuntimeAdapter().eval( runtime, bootstrapSetup );
 
-        IRubyObject hello = (IRubyObject) JavaEmbedUtils.invokeMethod( runtime, rootRubyObject, "execute", new Object[] {"Anda"}, IRubyObject.class );
-        System.out.println(JavaEmbedUtils.invokeMethod( runtime, hello, "start", new Object[] {params}, IRubyObject.class ));
+        IRubyObject setup = (IRubyObject) JavaEmbedUtils.invokeMethod( runtime, rootRubyObject, "execute", new Object[] {params}, IRubyObject.class );
+        IRubyObject players = (IRubyObject) JavaEmbedUtils.invokeMethod( runtime, setup, "create_players", new Object[] {}, IRubyObject.class );
+        System.out.println(players);
 
+        IRubyObject rootGame = JavaEmbedUtils.newRuntimeAdapter().eval( runtime, bootstrapGame );
+        IRubyObject game = (IRubyObject) JavaEmbedUtils.invokeMethod( runtime, rootGame, "execute", new Object[] {players, size}, IRubyObject.class );
+        System.out.println((IRubyObject) JavaEmbedUtils.invokeMethod( runtime, game, "markers", new Object[] {}, IRubyObject.class ));
+        System.out.println((IRubyObject) JavaEmbedUtils.invokeMethod( runtime, game, "size", new Object[] {}, IRubyObject.class ));
+        System.out.println(game);
 
         response.setStatusCode("OK");
         response.setResponseBody(index.homePageHtml());
